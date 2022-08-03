65c86f6f2ea4707287090c58f752fa76af66e761
TalitaMGR <litagrezz@gmail.com>
2022-08-02 21:34:01 -0300

    modal responsive in progress

--- Js/library.js
+++ Js/library.js
@@ -2,6 +2,8 @@ var cards= document.querySelector('.cardsBooks')
 var divModal = document.querySelector('.no-modal')
 var inputSearch= document.querySelector("#searchTitle")
 var inputSelect = document.querySelector('select')
+var divModal = document.getElementById('modal')
+var divModalResponsive = document.querySelector('.scrollModal')
 let data;
 fetch('../data.json').then(response=>{
     data= response.json()
@@ -57,9 +59,70 @@ function searchBook(){
 
 }
 
+function closeModal(){
+    divModal.classList.remove('modal')
+    divModal.classList.add('no-modal')
+}
+
 function openModal(index){
     divModal.classList.remove('no-modal')
     divModal.classList.add('modal')
+    divModalResponsive.textContent= ''
+    var modal = document.createElement('div')
+    modal.classList.add('modalElements')
+    buttonConstructor("btnClose", "X", closeModal, "", modal)
+    var img = document.createElement('img');
+    img.src=data.books[index].image
+    modal.appendChild(img);
+    var divInformations = document.createElement('div')
+    var title = document.createElement('h2')
+    var titleContent = document.createTextNode(data.books[index].tittle)
+    title.appendChild(titleContent)
+    divInformations.appendChild(title)
+    modal.appendChild(divInformations)
+
+    modalConstructor('Sinopse', 'synopsis', index, divInformations)
+    modalConstructor('Autor', 'author', index, divInformations)
+    modalConstructor('Gênero', 'genre', index, divInformations)
+    modalConstructor('Data de entrada', 'systemEntryDate', index, divInformations)
+    buttonConstructor("btnRent", "Emprestar", rentBook, index, modal)
+    buttonConstructor("btnEdit", "Editar", editBook, index, modal)
+    buttonConstructor("btnDelete", "Inativar", deleteBook, index, modal )
+    buttonConstructor("btnHistoric", "Histórico", historicBook, index, modal)
     
+    divModalResponsive.appendChild(modal)
 }
-    
+    function modalConstructor(subtitle, item, index, modal){
+        var sub = document.createElement('h4')
+        var subCategory = document.createTextNode(subtitle)
+        sub.appendChild(subCategory)
+        modal.appendChild(sub)
+        var subContent = document.createElement('p')
+        var subText = document.createTextNode(data.books[index][item])
+        subContent.appendChild(subText)
+        modal.appendChild(subContent)
+    }
+
+    function buttonConstructor(btnClass, btnName, btnFunction, index, modal){
+        var btn = document.createElement('button')
+        btn.classList.add(btnClass)
+        var btnName = document.createTextNode(btnName)
+        btn.appendChild(btnName)
+        btn.onclick = ()=> btnFunction(index)
+        modal.appendChild(btn)
+    }
+
+    function rentBook(index){
+        divModal.classList.remove('modal')
+        divModal.classList.add('no-modal')
+    }
+    function editBook(index){
+        divModal.classList.remove('modal')
+        divModal.classList.add('no-modal')
+    }
+    function deleteBook(index){
+
+    }
+    function historicBook(index){
+
+    }
