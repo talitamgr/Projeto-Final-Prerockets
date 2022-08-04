a0167eef88eae298d9b80611bd9d2bf11d9f2199
TalitaMGR <litagrezz@gmail.com>
2022-08-03 22:31:25 -0300

    working

--- Js/library.js
+++ Js/library.js
@@ -1,128 +1,63 @@
-var cards= document.querySelector('.cardsBooks')
+var cards = document.querySelector('.cardsBooks')
 var divModal = document.querySelector('.no-modal')
-var inputSearch= document.querySelector("#searchTitle")
+var inputSearch = document.querySelector("#searchTitle")
 var inputSelect = document.querySelector('select')
-var divModal = document.getElementById('modal')
-var divModalResponsive = document.querySelector('.scrollModal')
+
 let data;
-fetch('../data.json').then(response=>{
-    data= response.json()
+
+fetch('../data.json').then(response => {
+    data = response.json()
     return data
-}).then(body=>{
+}).then(body => {
     data = body.data
     var books = data.books
-    for(var i = 0; i < books.length; i++){
+    for (var i = 0; i < books.length; i++) {
         showElements(books[i], i)
-    }   
+    }
 })
 
-function showElements(item, index){
-var card = document.createElement('div')
-card.classList.add('card')
-var img = document.createElement('img');
-img.src=item.image
-card.appendChild(img);
-card.onclick = ()=>openModal(index)
-var p = document.createElement('p')
-var title = document.createTextNode(item.tittle)
-p.appendChild(title)
-card.appendChild(p)
-cards.appendChild(card)
-}        
-function searchBook(){
-    cards.textContent= " "
-    var noMatch= true;
-    var entrySearch= new RegExp(inputSearch.value, 'i')
-    if(inputSelect.value){
+function showElements(item, index) {
+    var card = document.createElement('div')
+    card.classList.add('card')
+    var img = document.createElement('img');
+    img.src = item.image
+    card.appendChild(img);
+    card.onclick = () => checkBook(index)
+    var p = document.createElement('p')
+    var title = document.createTextNode(item.tittle)
+    p.appendChild(title)
+    card.appendChild(p)
+    cards.appendChild(card)
+}
+
+function searchBook() {
+    cards.textContent = " "
+    var noMatch = true;
+    var entrySearch = new RegExp(inputSearch.value, 'i')
+    if (inputSelect.value) {
         var filterToUse = inputSelect.value;
-        for(var i=0;i < data.books.length; i++){
+        for (var i = 0; i < data.books.length; i++) {
             var compared = entrySearch.test(data.books[i][filterToUse]);
-            if(compared){
+            if (compared) {
                 showElements(data.books[i])
                 noMatch = false;
-            }}
-    }else{
-    for(var i=0;i < data.books.length; i++){
-    var compareTitle = entrySearch.test(data.books[i].tittle);
-    var compareGenre= entrySearch.test(data.books[i].genre);
-    var compareAuthor = entrySearch.test(data.books[i].author);
-    var compareDate = entrySearch.test(data.books[i].systemEntryDate)
-    if(compareTitle || compareAuthor || compareGenre || compareDate){
-        showElements(data.books[i])
-        noMatch = false;
+            }
+        }
+    } else {
+        for (var i = 0; i < data.books.length; i++) {
+            var compareTitle = entrySearch.test(data.books[i].tittle);
+            var compareGenre = entrySearch.test(data.books[i].genre);
+            var compareAuthor = entrySearch.test(data.books[i].author);
+            var compareDate = entrySearch.test(data.books[i].systemEntryDate)
+            if (compareTitle || compareAuthor || compareGenre || compareDate) {
+                showElements(data.books[i])
+                noMatch = false;
+            }
+        }
     }
-    }}
-        
-    if(noMatch){
-        cards.textContent= " Título não encontrado"
+
+    if (noMatch) {
+        cards.textContent = " Título não encontrado"
     }
 
 }
-
-function closeModal(){
-    divModal.classList.remove('modal')
-    divModal.classList.add('no-modal')
-}
-
-function openModal(index){
-    divModal.classList.remove('no-modal')
-    divModal.classList.add('modal')
-    divModalResponsive.textContent= ''
-    var modal = document.createElement('div')
-    modal.classList.add('modalElements')
-    buttonConstructor("btnClose", "X", closeModal, "", modal)
-    var img = document.createElement('img');
-    img.src=data.books[index].image
-    modal.appendChild(img);
-    var divInformations = document.createElement('div')
-    var title = document.createElement('h2')
-    var titleContent = document.createTextNode(data.books[index].tittle)
-    title.appendChild(titleContent)
-    divInformations.appendChild(title)
-    modal.appendChild(divInformations)
-
-    modalConstructor('Sinopse', 'synopsis', index, divInformations)
-    modalConstructor('Autor', 'author', index, divInformations)
-    modalConstructor('Gênero', 'genre', index, divInformations)
-    modalConstructor('Data de entrada', 'systemEntryDate', index, divInformations)
-    buttonConstructor("btnRent", "Emprestar", rentBook, index, modal)
-    buttonConstructor("btnEdit", "Editar", editBook, index, modal)
-    buttonConstructor("btnDelete", "Inativar", deleteBook, index, modal )
-    buttonConstructor("btnHistoric", "Histórico", historicBook, index, modal)
-    
-    divModalResponsive.appendChild(modal)
-}
-    function modalConstructor(subtitle, item, index, modal){
-        var sub = document.createElement('h4')
-        var subCategory = document.createTextNode(subtitle)
-        sub.appendChild(subCategory)
-        modal.appendChild(sub)
-        var subContent = document.createElement('p')
-        var subText = document.createTextNode(data.books[index][item])
-        subContent.appendChild(subText)
-        modal.appendChild(subContent)
-    }
-
-    function buttonConstructor(btnClass, btnName, btnFunction, index, modal){
-        var btn = document.createElement('button')
-        btn.classList.add(btnClass)
-        var btnName = document.createTextNode(btnName)
-        btn.appendChild(btnName)
-        btn.onclick = ()=> btnFunction(index)
-        modal.appendChild(btn)
-    }
-
-    function rentBook(index){
-        divModal.classList.remove('modal')
-        divModal.classList.add('no-modal')
-    }
-    function editBook(index){
-        divModal.classList.remove('modal')
-        divModal.classList.add('no-modal')
-    }
-    function deleteBook(index){
-
-    }
-    function historicBook(index){
-
-    }
---
+++ Js/modal.js
@@ -0,0 +1,270 @@
+var divModal = document.getElementById('modal')
+var divModalResponsive = document.querySelector('.scrollModal')
+var divModalRent = document.querySelector('.no-modalRent')
+var divModalReturn = document.querySelector('.no-modalReturn')
+var divScrollModalReturn = document.querySelector('.scrollModalReturn')
+
+function checkBook(index) {
+    var rentHistory = data.books[index].rentHistory
+
+    if (data.books[index].status.isActive) {
+        if (rentHistory.length == 0) {
+            openModal(index)
+        } else {
+            var today = new Date();
+            var date = formatDate(rentHistory[rentHistory.length - 1].deliveryDate)
+            var deliveryDate = new Date(date);
+            deliveryDate.setHours(deliveryDate.getHours() + 3);
+            if (today < deliveryDate) {
+                reshowModal(index)
+            } else {
+                openModal(index)
+            }
+        }
+    }
+}
+
+function formatDate(date) {
+    var separate = date.split('/')
+    var newDate = separate[2] + '-' + separate[1] + '-' + separate[0]
+    return newDate
+}
+
+function closeModal() {
+    divModal.classList.remove('modal')
+    divModal.classList.add('no-modal')
+}
+
+function openModal(index) {
+    divModal.classList.remove('no-modal')
+    divModal.classList.add('modal')
+    divModalResponsive.textContent = ''
+    var modal = document.createElement('div')
+    modal.classList.add('modalElements')
+    buttonConstructor("btnClose", "X", closeModal, "", modal)
+    var img = document.createElement('img');
+    img.src = data.books[index].image;
+    modal.appendChild(img);
+    var divInformations = document.createElement('div')
+    var title = document.createElement('h2')
+    var titleContent = document.createTextNode(data.books[index].tittle)
+    title.appendChild(titleContent)
+    divInformations.appendChild(title)
+    modal.appendChild(divInformations)
+
+    modalConstructor('Sinopse', 'synopsis', index, divInformations)
+    modalConstructor('Autor', 'author', index, divInformations)
+    modalConstructor('Gênero', 'genre', index, divInformations)
+    modalConstructor('Data de entrada', 'systemEntryDate', index, divInformations)
+    buttonConstructor("btnRent", "Emprestar", rentBook, index, modal)
+    buttonConstructor("btnEdit", "Editar", editBook, index, modal)
+    buttonConstructor("btnDelete", "Inativar", deleteBook, index, modal)
+    buttonConstructor("btnHistoric", "Histórico", historicBook, index, modal)
+
+    divModalResponsive.appendChild(modal)
+}
+
+function modalConstructor(subtitle, item, index, modal) {
+    var sub = document.createElement('h4')
+    var subCategory = document.createTextNode(subtitle)
+    sub.appendChild(subCategory)
+    modal.appendChild(sub)
+    var subContent = document.createElement('p')
+    var subText = document.createTextNode(data.books[index][item])
+    subContent.appendChild(subText)
+    modal.appendChild(subContent)
+}
+
+function buttonConstructor(btnClass, btnName, btnFunction, index, modal) {
+    var btn = document.createElement('button')
+    btn.classList.add(btnClass)
+    var btnName = document.createTextNode(btnName)
+    btn.appendChild(btnName)
+    btn.onclick = () => btnFunction(index)
+    modal.appendChild(btn)
+}
+
+function rentBook(index) {
+    divModal.classList.remove('modal')
+    divModal.classList.add('no-modal')
+    divModalRent.classList.remove('no-modalRent')
+    divModalRent.classList.add('modalRent')
+    var btnSaveRent = document.querySelector(".saveRent")
+    btnSaveRent.onclick = () => saveRent(index)
+}
+
+function saveRent(i) {
+    var nameInput = document.getElementById('nameStudent')
+    var classInput = document.getElementById('classStudent')
+    var dateLoan = document.getElementById('dateLoan')
+    var dateDelivery = document.getElementById('dateDelivery')
+
+    var dateL = new Date(dateLoan.value)
+    var dateLoanConfig = setDate(dateL, true)
+    var dateD = new Date(dateDelivery.value)
+    var dateDeliveryConfig = setDate(dateD, true)
+    data.books[i].rentHistory.push(
+        {
+            "studentName": nameInput.value,
+            "class": classInput.value,
+            "withdrawalDate": dateLoanConfig,
+            "deliveryDate": dateDeliveryConfig
+        }
+    )
+    // var file = new Blob([JSON.stringify({data})], {type: "text/plain"});
+
+    // const saveFile = async blob => {
+    //     try {
+    //         const handle = await window.showSaveFilePicker({
+    //         suggestedName: 'data.json',
+    //         types: [
+    //             {
+    //             description: "Json",
+    //             accept: { "text/plain": [".json"] },
+    //             },
+    //         ],
+    //         })
+
+    //         const writable = await handle.createWritable()
+    //         await writable.write(blob)
+    //         await writable.close()
+
+    //         return handle
+    //     } catch (err) {
+    //         console.error(err.name, err.message)
+    //     }
+    // }
+
+    // saveFile(file)
+    reshowModal(i)
+}
+
+function setDate(date, shouldAddHours) {
+    if (shouldAddHours) {
+        date.setHours(date.getHours() + 3);
+    }
+    var day = date.getDate();
+    day = day.toString().padStart(2, 0);
+    var month = (date.getMonth() + 1);
+    month = month.toString().padStart(2, 0);
+    var year = date.getFullYear();
+    date = day + '/' + month + '/' + year;
+    return date
+}
+
+function closeModalRent() {
+    divModalRent.classList.remove('modalRent')
+    divModalRent.classList.add('no-modalRent')
+    divModal.classList.remove('no-modal')
+    divModal.classList.add('modal')
+}
+
+function reshowModal(i) {
+    divModalRent.classList.remove('modalRent')
+    divModalRent.classList.add('no-modalRent')
+    divModalReturn.classList.remove('no-modalReturn')
+    divModalReturn.classList.add('modalReturn')
+    divScrollModalReturn.textContent = ''
+    var modal = document.createElement('div')
+    modal.classList.add('modalReturnBook')
+    var modalBook = document.createElement('div')
+    modalBook.classList.add('modalElements')
+    buttonConstructor("btnClose", "X", closeModalReturn, "", modalBook)
+    var img = document.createElement('img');
+    img.src = data.books[i].image
+    modalBook.appendChild(img);
+    var divInformations = document.createElement('div')
+    var title = document.createElement('h2')
+    var titleContent = document.createTextNode(data.books[i].tittle)
+    title.appendChild(titleContent)
+    divInformations.appendChild(title)
+    modalBook.appendChild(divInformations)
+
+    modalConstructor('Sinopse', 'synopsis', i, divInformations)
+    modalConstructor('Autor', 'author', i, divInformations)
+    modalConstructor('Gênero', 'genre', i, divInformations)
+    modalConstructor('Data de entrada', 'systemEntryDate', i, divInformations)
+    buttonConstructor("btnReturn", "Devolver", returnBook, i, modalBook)
+    buttonConstructor("btnEdit", "Editar", editBook, i, modalBook)
+    buttonConstructor("btnDelete", "Inativar", deleteBook, i, modalBook)
+    buttonConstructor("btnHistoric", "Histórico", historicBook, i, modalBook)
+
+    modal.appendChild(modalBook)
+    divScrollModalReturn.appendChild(modal)
+    var rentInformations = document.createElement('div')
+    rentInformations.classList.add('rentInformations')
+    var datas = document.createElement('h2')
+    var datasContent = document.createTextNode('Dados do aluno')
+    datas.appendChild(datasContent)
+    modal.appendChild(datas)
+    modal.appendChild(rentInformations)
+    modalRentConstructor('Nome do aluno', 'studentName', i, rentInformations)
+    modalRentConstructor('Sala', 'class', i, rentInformations)
+    modalRentConstructor('Data da retirada', 'withdrawalDate', i, rentInformations)
+    modalRentConstructor('Data da entrega', 'deliveryDate', i, rentInformations)
+
+}
+
+function modalRentConstructor(subtitle, item, index, modal) {
+    var rentHistory = data.books[index].rentHistory
+    var divFlex = document.createElement('div')
+    divFlex.classList.add('divFlex')
+    var sub = document.createElement('h4')
+    var subCategory = document.createTextNode(subtitle)
+    sub.appendChild(subCategory)
+    divFlex.appendChild(sub)
+    var subContent = document.createElement('p')
+    var subText = document.createTextNode(rentHistory[rentHistory.length - 1][item])
+    subContent.appendChild(subText)
+    divFlex.appendChild(subContent)
+    modal.appendChild(divFlex)
+}
+
+function closeModalReturn() {
+    divModalReturn.classList.remove('modalReturn')
+    divModalReturn.classList.add('no-modalReturn')
+}
+
+function returnBook(index) {
+    var today = new Date()
+    var date = setDate(today, false)
+    data.books[index].rentHistory[data.books[index].rentHistory.length - 1].deliveryDate = date;
+    // var file = new Blob([JSON.stringify({ data })], { type: "text/plain" });
+
+    //     const saveFile = async blob => {
+    //         try {
+    //             const handle = await window.showSaveFilePicker({
+    //             suggestedName: 'data.json',
+    //             types: [
+    //                 {
+    //                     description: "Json",
+    //                     accept: { "text/plain": [".json"] },
+    //                 },
+    //             ],
+    //         })
+
+    //         const writable = await handle.createWritable()
+    //         await writable.write(blob)
+    //         await writable.close()
+
+    //         return handle
+    //     } catch (err) {
+    //         console.error(err.name, err.message)
+    //     }
+    // }
+    // saveFile(file)
+    divModalReturn.classList.remove('modalReturn')
+    divModalReturn.classList.add('no-modalReturn')
+    openModal(index)
+}
+
+function editBook(index) {
+    divModal.classList.remove('modal')
+    divModal.classList.add('no-modal')
+}
+function deleteBook(index) {
+
+}
+function historicBook(index) {
+
+}
